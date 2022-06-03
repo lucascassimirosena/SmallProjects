6f22fb02c3951f1f5eab2f4f35c36cd3e3e1416b
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-06-03 19:39:24 -0300

    working on it

---
+++ MemoryMap/main.c
@@ -0,0 +1,247 @@
+#include <stdio.h>
+
+/*
+0 - s, ss, z, c    6 - sh, x, ch
+1 - t, d           7 - k, g
+2 - n              8 - f, v
+3 - m              9 - p, b
+4 - r             
+5 - l             
+
+*/
+
+
+int main()
+{
+
+    int i = 1;
+    while(i < 10)
+    {
+        int dice, dodo, tin, dome, door, tail, dish, dog, dove, tape;
+        printf("Me fala o que e 'dice': ");
+        scanf("%d", &dice);
+
+        if(dice == 10)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+        printf("E o que e 'dodo': ");
+        scanf("%d", &dodo);
+        if(dodo == 11)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+        printf("E 'tin': ");
+        scanf("%d", &tin);
+        if(tin == 12)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+        printf("Legal, e o que e 'dome': ");
+        scanf("%d", &dome);
+        if(dome == 13)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+        printf("Ok. E 'door': ");
+        scanf("%d", &door);
+        if(door == 14)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+        printf("E o que e 'tail': ");
+        scanf("%d", &tail);
+
+        if(tail == 15)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+        printf("Qual e o numero de 'dish': ");
+        scanf("%d", &dish);
+
+        if(dish == 16)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+        printf("E o que e 'dog': ");
+        scanf("%d", &dog);
+
+        if(dog == 17)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+        printf("Ok. E 'dove': ");
+        scanf("%d", &dove);
+        
+        if(dove == 18)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+        printf("Me fala o que e 'tape': ");
+        scanf("%d", &tape);
+
+        if(tape == 19)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+    }
+
+    printf("Nível 2, você está pronto?\n :  ");
+
+    i = 2;
+    while(i <= 9)
+    {
+        int dice, dodo, tin, dome, door, tail, dish, dog, dove, tape;
+
+        printf("Me fala o que e 'tape': ");
+        scanf("%d", &tape);
+
+        if(tape == 19)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+        printf("Ok. E 'dove': ");
+        scanf("%d", &dove);
+        
+        if(dove == 18)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+        printf("E o que e 'tail': ");
+        scanf("%d", &tail);
+
+        if(tail == 15)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+        printf("Me fala o que e 'dice': ");
+        scanf("%d", &dice);
+
+        if(dice == 10)
+        {
+            printf("Correto.\n");
+            i++;
+        }
+        else
+        {
+            printf("Errado.\n");
+            i--;
+        }
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return (0);
+}
